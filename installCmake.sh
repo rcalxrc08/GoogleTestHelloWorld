2c2f795bb2c728a53bb726ab701aba07139bc525
rcalxrc08 <nicola.scaramuzzino@yandex.com>
2017-02-07 12:55:21 +0100

    unstable

--- installCmake.sh
+++ installCmake.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 wget https://cmake.org/files/v3.7/cmake-3.7.2.tar.gz
-tar -xzvf cmake-3.7.2.tar.gz
+tar -xzvf cmake-3.7.2.tar.gz > /dev/null
 cd cmake-3.7.2/
 cmake . > /dev/null
 make > /dev/null    
